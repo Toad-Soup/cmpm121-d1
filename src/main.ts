30789b524049a4fb9d1723f4368ce862d76c279b
torrey <113153846+Toad-Soup@users.noreply.github.com>
2025-10-07 16:42:43 +0000

    debugging git

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+// Simple counter for demonstration
+let counter: number = 0;
+
+// Create basic HTML structure
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>CD Player</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button class = "favorite styled"
+    id="increment">💿</button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  // increment the counter when the button is clicked
+  counter += 1;
+  counterElement.innerHTML = counter.toString();
+});
