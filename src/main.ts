c774e1d67cee83332d4ac5a61fc04f1b186c29e4
torrey <113153846+Toad-Soup@users.noreply.github.com>
2025-10-08 02:28:31 +0000

    added an auto clicker button (step 5) and a bit of juice

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,28 @@ let counter: number = 0;
 document.body.innerHTML = `
   <h1>CD Player</h1>
   <p>Counter: <span id="counter">0</span></p>
-  <button class = "favorite styled"
-    id="increment">💿</button>
+  <div class = "button-container">
+    <button class = "favorite styled" id="increment">💿</button>
+    <button class = "auto" id ="auto">Auto Click</button>
+  <div>
 `;
 
 // const click handler
 const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
+const autoClicker = document.getElementById("auto")!;
 
 //add the ability to click the button
 button.addEventListener("click", () => {
   // increment the counter when the button is clicked
   counter += 1;
   counterElement.innerHTML = counter.toString();
+  button.classList.add("spin");
+
+  //stop the animation so that it can spin again when clicked
+  button.addEventListener("animationend", () => {
+    button.classList.remove("spin");
+  }, { once: true }); // 'once: true' ensures the event listener runs only once
 });
 
 /*step 3 increase coiunt by 1 each second using setInterval
@@ -30,16 +39,23 @@ setInterval(() => {
 }, 1000); //1000 milliseconds in a second, had to google that :p
 */
 
-//step 4 increment count every second based on frame rate
-let lastTime = performance.now();
+let lastTime: number = 0;
 
-requestAnimationFrame(animate);
+autoClicker.addEventListener("click", () => {
+  if (counter >= 10) {
+    lastTime = performance.now();
+    requestAnimationFrame(animate);
+  }
+});
+
+//requestAnimationFrame(animate);
 function animate(timestamp: number) {
   if (lastTime != null) { //make sure that time has passed and is valid
     //calulate delta time for times passed, refrence: https://stackoverflow.com/questions/26576625/how-can-i-correctly-calculate-the-time-delta
     const value = (timestamp - lastTime) / 1000; //take the current time, subtract the last time, and divide it by one second to find delta
     counter += value; //increment the counter by the delta time that has passed based on framerate (so like 1/60th if 60 fps)
     counterElement.innerHTML = counter.toFixed(0); //increment by 1 per second but the value is a float it needs to be fixed so it doesnt look bad
+    button.classList.add("infinite-spin");
   }
 
   lastTime = timestamp;
